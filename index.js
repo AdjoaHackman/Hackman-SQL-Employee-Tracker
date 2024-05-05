4fa4ba32ede880e24f7548574e40a69bffcb14b0
Adjoa Hackman <adjoahackman93@gmail.com>
2024-05-05 10:50:53 -0700

    trying to figure out the final query

--- index.js
+++ index.js
@@ -64,87 +64,122 @@ function init() {
                             )
                         })
                     });
-                if (answers.options === 'Add an employee') {
-                    inquirer.prompt([
-                        {
-                            type: 'input',
-                            name: 'first',
-                            message: 'What is the first name of the employee?',
-                        },
-                        {
-                            type: 'input',
-                            name: 'last',
-                            message: 'What is the last name of the employee?',
-                        },
-                        {
-                            type: 'input',
-                            name: 'role',
-                            message: 'What is the name of the role?',
-                        },
-                        {
-                            type: 'input',
-                            name: 'manager',
-                            message: 'What is the manager id for the employee',
-                        },
-                    ])
-                        .then((data) => {
-                            connection.query('INSERT INTO employee,(first name, last name, role_id, manager_id) VALUES (?, ?, ?, ?)', [data.role, data.salary, data.department_id], function (err, results) {
-                                connection.query(`SELECT * from employee`, function (err, results) {
-                                    console.table(results)
-                                    init()
-                                }
-                                )
-                            });
-                        })
-                    if (answers.options === 'Add a department') {
-                        inquirer.prompt([
-                            {
-                                type: 'input',
-                                name: 'department',
-                                message: 'What is the name of the department',
-                            },
-                        ])
-                            .then((data) => {
-                                connection.query(`INSERT INTO department (name) VALUES (?)`, data.department, function (err, results) {
-                                    connection.query(`SELECT * from department`, function (err, results) {
-                                        console.table(results)
-                                        init()
-                                    })
-                                }
-                                )
-                            })
-
-                    };
-                    if (answers.options === 'Update an employee role') {
-                        inquirer.prompt([
-                            {
-                                type: 'input',
-                                name: 'first',
-                                message: 'What is the first name of the employee?',
-                            },
-                            {
-                                type: 'input',
-                                name: 'last',
-                                message: 'What is the last name of the employee?',
-                            },
-                            {
-                                type: 'input',
-                                name: 'role',
-                                message: 'What is the name of the role?',
-                            },
-                        ])
-                        .then((data) => {
-                        connection.query('UPDATE (role) FROM employee VALUES (?,?,?)', function (err, results) {
-                            connection.query(`SELECT * from role`, function (err, results) {
+            }
+            if (answers.options === 'Add an employee') {
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'first',
+                        message: 'What is the first name of the employee?',
+                    },
+                    {
+                        type: 'input',
+                        name: 'last',
+                        message: 'What is the last name of the employee?',
+                    },
+                    {
+                        type: 'input',
+                        name: 'role',
+                        message: 'What is the name of the role?',
+                    },
+                    {
+                        type: 'input',
+                        name: 'manager',
+                        message: 'What is the manager id for the employee',
+                    },
+                ])
+                    .then((data) => {
+                        connection.query('INSERT INTO employee,(first name, last name, role_id, manager_id) VALUES (?, ?, ?, ?)', [data.role, data.salary, data.department_id], function (err, results) {
+                            connection.query(`SELECT * from employee`, function (err, results) {
                                 console.table(results)
                                 init()
                             }
                             )
-                        })});
+                        });
+                    })
+            }
+            if (answers.options === 'Add a department') {
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'department',
+                        message: 'What is the name of the department',
+                    },
+                ])
+                    .then((data) => {
+                        connection.query(`INSERT INTO department (name) VALUES (?)`, data.department, function (err, results) {
+                            connection.query(`SELECT * from department`, function (err, results) {
+                                console.table(results)
+                                init()
+                            })
+                        }
+                        )
+                    })
 
-                    }
-                }
+            };
+            // async function getEmployees(cb) {
+            //     cb([
+            //         {
+            //             name: 'TEster',
+            //             id: 1,
+            //         },
+            //         {
+            //             name: 'test2',
+            //             id: 2,
+            //         },
+            //     ]);
+            // }
+            
+            // async function getRoles(cb) {
+            //     cb([
+            //         {
+            //             name: 'Director',
+            //             id: 1,
+            //         },
+            //         {
+            //             name: 'Employee',
+            //             id: 2,
+            //         },
+            //     ]);
+            // }
+            
+            getEmployees((employees) => {
+                getRoles((roles) => {
+            if (answers.options === 'Update an employee role') {
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'employee',
+                        message: 'Select an employee?',
+                        choices: employees.map((employee) => {
+                            return {
+                                name: employee.name,
+                                value: employee.id,
+                            };
+                        })
+                    },
+                    {
+                        type: 'list',
+                        name: 'role',
+                        message: 'Select a role',
+                        choices: roles.map((role) => {
+                            return {
+                                name: role.name,
+                                value: role.id,
+                            };
+                        }),
+                    },
+                ]).then((data) => {
+                        const {employee, role} = data;
+                        console.table(employee,role);
+                        const query = "UPDATE employee SET role_id = ? WHERE id = ?";
+                        db.query(query, [role, employee], (err, res) => {
+
+                        });
+                    });
             }
         })
-}
+            })
+
+        }}
 init()
